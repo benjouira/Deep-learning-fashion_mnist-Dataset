cb641b6e8fd57ac067be30d4249b2c88ea3d1fd1
Rabii <rabi3jouira@gmail.com>
2021-12-06 22:28:36 +0100

    Create fashion_mnist.py

---
+++ fashion_mnist.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix,classification_report, accuracy_score
+
+
+print(tf.__version__)
+
+
+# ********************************************************************
